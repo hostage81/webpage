45c814e3be246ae8f0abb03fd2e353396966455e
ahmed nasser <ah.na@windowslive.com>
2022-09-30 22:56:57 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//execute script after load 
+window.onload = function digital_fn(){
+//toggle button
+let toggle=document.querySelector("#nav .toggle-btn")
+let collapse=document.querySelector("#nav .collapse")
+
+toggle.addEventListener('click',function(event){
+    collapse.classList.toggle('active')
+//console.log(toggle)
+});
+}
